c21c9e260ec62c34d474aa327e5091329aef9787
AM111D <86677972+AM111D@users.noreply.github.com>
2023-03-07 20:37:53 +0000

    Deploying to gh-pages from @ AM111D/goit-js-hw-11@7300925017ede50c8292e6001d1ba224a3856897 🚀

--- index.362f3bbd.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.elements.searchQuery.value}));
-//# sourceMappingURL=index.362f3bbd.js.map
--- index.b194a22e.js
+++
@@ -1,2 +0,0 @@
-({searchForm:document.querySelector("#search-form")}).searchForm.addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.elements.searchQuery.value}));
-//# sourceMappingURL=index.b194a22e.js.map
